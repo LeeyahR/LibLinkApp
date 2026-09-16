442abfacfcf913ec8b75c81aca4ae296db858886
LeeyahR <ST10440018@vcconnect.edu.za>
2026-09-16 23:57:19 +0200

    Added Profile Activity and view

--- app/src/main/java/com/example/liblinkapp/ProfileActivity.kt
+++ app/src/main/java/com/example/liblinkapp/ProfileActivity.kt
@@ -1,16 +1,69 @@
 package com.example.liblinkapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var editName : EditText
+    private lateinit var editEmail : EditText
+    private lateinit var btnSaveProfile : Button
+    private lateinit var btnLogout : Button
+
+    private lateinit var navHome : TextView
+    private lateinit var navBrowse : TextView
+    private lateinit var navMyBooks : TextView
+    private lateinit var navProfile : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
+
+        editName = findViewById(R.id.editName)
+        editEmail = findViewById(R.id.editEmail)
+        btnSaveProfile = findViewById(R.id.btnSaveProfile)
+        btnLogout = findViewById(R.id.btnLogout)
+
+        btnSaveProfile.setOnClickListener {
+            Toast.makeText(this, "Profile updated successfully", Toast.LENGTH_LONG).show()
+        }
+
+        btnLogout.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            finish()
+        }
+
+        //bottom nav
+        navHome.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
+
+        navBrowse.setOnClickListener {
+            startActivity(Intent(this, BrowseActivity::class.java))
+            finish()
+        }
+
+        navMyBooks.setOnClickListener {
+            startActivity(Intent(this, MyBooksActivity::class.java))
+        }
+
+        navProfile.setOnClickListener {
+            //Already here
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
