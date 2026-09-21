13973b686a6cb6c60213449265e0d12f13b8229c
SaihilNarine <St10440685@vcconnect.edu.za>
2026-09-21 07:51:19 +0200

    Updated program.cs to ensure api is functioning

--- LibLink_API/LibLink_API/Program.cs
+++ LibLink_API/LibLink_API/Program.cs
@@ -6,15 +6,17 @@ public partial class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        var connectionString =
-            builder.Configuration.GetConnectionString("LibLink_APIContext")
-            ?? throw new InvalidOperationException(
-                "Connection string 'LibLink_APIContext' not found.");
+        var connectionString = builder.Configuration.GetConnectionString("LibLink_APIContext")
+            ?? throw new InvalidOperationException("Connection string 'LibLink_APIContext' not found.");
 
         builder.Services.AddDbContext<LibLink_APIContext>(options =>
             options.UseSqlServer(connectionString));
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler =
+                System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        });
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
